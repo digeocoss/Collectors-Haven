2417c61be0eeb0ac3ea57b82b064523973e52013
Alejandro-Bernal <alejandro.bernal@urbantxt.com>
2022-08-10 15:01:15 -0700

    Finished styling confirmation page, finished app flow.

--- components/Confirmation Screen/Confirmation.tsx
+++ components/Confirmation Screen/Confirmation.tsx
@@ -6,6 +6,7 @@ import {
   FlatList,
   TouchableOpacity,
 } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 import { RootStackScreenProps } from "../../types";
 import { styles } from "./ConfirmationStyles";
 
@@ -14,15 +15,49 @@ export default function Confirmation({
   route,
 }: RootStackScreenProps<"ConfirmationPage">) {
   const submission: any = route.params;
+  console.log(submission);
 
   return (
     <View style={styles.container}>
-      <View >
-        <View >
-          <Image
-            source={{ uri: submission.data.img }}
-            style={{ height: 176, width: 178, borderRadius: 10 }}
-          />
+      <View style={styles.mainContentContainer}>
+        <View style={styles.confirmedContainer}>
+          <View>
+            <AntDesign name="checkcircleo" size={200} color="white" />
+          </View>
+          <View>
+            <Text style={styles.confirmedText}>Posted!</Text>
+          </View>
+          <View>
+            <Image
+              source={{ uri: submission.data.img }}
+              style={{ height: 176, width: 178, borderRadius: 10 }}
+            />
+          </View>
+        </View>
+        <View style={styles.itemDescriptionContainer}>
+          <View>
+            <Text style={styles.itemTitle}>{submission.data.title}</Text>
+          </View>
+          <View>
+            <Text style={styles.itemDescription}>
+              {submission.data.description}
+            </Text>
+          </View>
+        </View>
+        <View style={styles.backHome}>
+          <TouchableOpacity
+            style={{
+              backgroundColor: "white",
+              borderRadius: 10,
+              width: 150,
+              height: 25,
+            }}
+            onPress={() => {
+              navigation.navigate("Feed");
+            }}
+          >
+            <Text style={{ fontSize: 20, textAlign: "center" }}> Go Home</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </View>
--- components/Confirmation Screen/ConfirmationStyles.tsx
+++ components/Confirmation Screen/ConfirmationStyles.tsx
@@ -4,6 +4,36 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#084F6D",
+    justifyContent: "center",
+  },
+  mainContentContainer: {
+    marginTop: "10%",
+  },
+  confirmedContainer: {
+    alignItems: "center",
+  },
+  confirmedText: {
+    color: "#fff",
+    fontSize: 55,
+    textAlign: "center",
+    marginTop: 20,
+    marginBottom: 30,
+  },
+  itemDescriptionContainer: {
+    paddingTop: 15,
+    alignItems: "center",
+  },
+  itemTitle: {
+    color: "#fff",
+    paddingBottom: 20,
+    fontSize: 30,
+    fontWeight: "bold",
+  },
+  itemDescription: { color: "#fff", fontSize: 20 },
+  backHome: {
+    alignItems: "center",
+    marginTop: 40,
+    justifyContent: "center",
   },
 });
 
